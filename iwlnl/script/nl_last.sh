a18a0051d47cce56c623d55893b8e7310aae9d50
iamhyc <sudofree@163.com>
2018-07-30 19:36:44 +0800

    update iwlnl script

--- iwlnl/script/nl_last.sh
+++ iwlnl/script/nl_last.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./iwlnl_cmd --aifs 15 --cw_min 1023 --cw_max 2047 -i 0
-./iwlnl_cmd --aifs 15 --cw_min 1023 --cw_max 2047 -i 1
-./iwlnl_cmd --aifs 15 --cw_min 1023 --cw_max 2047 -i 2
-./iwlnl_cmd --aifs 15 --cw_min 1023 --cw_max 2047 -i 3
+./iwlnl_cmd --aifs 255 --cw_min 2047 --cw_max 8191 -i 0
+./iwlnl_cmd --aifs 255 --cw_min 2047 --cw_max 8191 -i 1
+./iwlnl_cmd --aifs 255 --cw_min 2047 --cw_max 8191 -i 2
+./iwlnl_cmd --aifs 255 --cw_min 2047 --cw_max 8191 -i 3
 echo "Slowest."
